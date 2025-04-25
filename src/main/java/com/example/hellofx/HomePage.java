6751183f4fd3a0e9ef328dd5af310080f45098fc
M.N.A. Nizaaq <it22894274@my.sliit.lk>
2025-04-25 11:22:55 +1000

    first commit (Home page)

---
+++ src/main/java/com/example/hellofx/HomePage.java
@@ -0,0 +1,25 @@
+package com.example.hellofx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomePage extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("/com/example/hellofx/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Image icon = new Image("Logo.png");
+        stage.getIcons().add(icon);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/hellofx/HomePageController.java
@@ -0,0 +1,23 @@
+package com.example.hellofx;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class HomePageController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+
+
+}
